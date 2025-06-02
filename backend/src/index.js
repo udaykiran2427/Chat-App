9337f658b59e779a91cd75b1ab5f2f63727bf5ea
udaykiran2427 <udaykiranpersonal77@gmail.com>
2025-06-02 23:08:12 +0530

    cors error fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,15 +12,14 @@ const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
-
-app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
 );
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
