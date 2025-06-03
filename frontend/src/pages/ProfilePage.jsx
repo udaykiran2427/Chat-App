ca800cf8e90e18bf836ab2efd54ced51fd762329
udaykiran2427 <udaykiranpersonal77@gmail.com>
2025-06-03 23:49:53 +0530

    set up of profile page

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { useAuthStore } from "../store/useAuthStore";
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
+  const handleImageUpload = async (e) => {};
+  return (
+    <div className="h-screen pt-20">
+      <div className="max-2-2xl mx-auto p-4 py-8">
+        <div className="bg-base-300 rounded-xl p-6 space-y-8">
+          <div className="text-center">
+            <h1 className="text-2xl font-semibold">Profile</h1>
+            <p className="mt-2">Your profile information</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
